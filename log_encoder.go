14090a3c616f4b69674926178ff82fd02c363a14
Adam Babik <a.babik@designfortress.com>
2019-07-23 18:55:24 +0200

    export RegisterJSONHexEncoder

--- log_encoder.go
+++ log_encoder.go
@@ -11,6 +11,10 @@ type jsonHexEncoder struct {
 	zapcore.Encoder
 }
 
+// NewJSONHexEncoder creates a JSON logger based on zapcore.NewJSONEncoder
+// but overwrites encoding of byte slices. Instead encoding them with base64,
+// jsonHexEncoder uses hex-encoding.
+// Each hex-encoded value is prefixed with 0x so that it's clear it's a hex string.
 func NewJSONHexEncoder(cfg zapcore.EncoderConfig) zapcore.Encoder {
 	jsonEncoder := zapcore.NewJSONEncoder(cfg)
 	return &jsonHexEncoder{
@@ -22,7 +26,10 @@ func (enc *jsonHexEncoder) AddBinary(key string, val []byte) {
 	enc.AddString(key, "0x"+hex.EncodeToString(val))
 }
 
-func registerJSONHexEncoder() error {
+// RegisterJSONHexEncoder registers a jsonHexEncoder under "json-hex" name.
+// Later, this name can be used as a value for zap.Config.Encoding to enable
+// jsonHexEncoder.
+func RegisterJSONHexEncoder() error {
 	return zap.RegisterEncoder("json-hex", func(cfg zapcore.EncoderConfig) (zapcore.Encoder, error) {
 		return NewJSONHexEncoder(cfg), nil
 	})
